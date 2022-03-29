1ccc8671fe44ad9ba97afdc9ac2f2da5ee9a998a
danielgarza22 <danielgarza22@gmail.com>
2022-03-28 20:46:34 -0400

    Update2

--- static/js/app.js
+++ static/js/app.js
@@ -10,16 +10,12 @@
 // Function that populates the metadata
 function demoInfo(sample)
 {
-    // console.log(sample);
     d3.json("samples.json").then((data) => {
         let metaData = data.metadata;
-        // console.log(metaData);
 
         let result = metaData.filter(sampleResult => sampleResult.id == sample);
-        // console.log(result);
 
         let resultData = result[0];
-        console.log(resultData);
 
         d3.select("#sample-metadata").html("");
 
@@ -31,24 +27,91 @@ function demoInfo(sample)
     });
 }
 
-// Function that build the graphs
+// Function that build bar chart
 function buildBarChart(sample)
 {
-    console.log(sample)
+    let data = d3.json("samples.json");
+
+    d3.json("samples.json").then((data) => {
+        let sampleData = data.samples;
+
+        let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+        
+        let resultData = result[0];
+
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+
+        // Bar chart
+        let yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`);
+        let xValues = sample_values.slice(0, 10);
+        let textLabels = otu_labels.slice(0, 10);
+
+        let barChart = {
+            y: yticks.reverse(),
+            x: xValues.reverse(),
+            text: textLabels.reverse(),
+            type: "bar",
+            orientation: "h"
+        }
+
+        let layout = {
+            title: "Top 10 Belly Button Bacteria"
+        };
+
+        Plotly.newPlot("bar", [barChart], layout)
+     
+    });
 }
 
+// Function that build bubble chart
+function buildBubbleChart(sample)
+{
+    let data = d3.json("samples.json");
+
+    d3.json("samples.json").then((data) => {
+        let sampleData = data.samples;
+
+        let result = sampleData.filter(sampleResult => sampleResult.id == sample);
+        
+        let resultData = result[0];
+
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+
+        // Bubble chart
+        let bubbleChart = {
+            y: sample_values,
+            x: otu_ids,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }
+
+        let layout = {
+            title: "Bacteria Cultures per Sample",
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"}
+        };
+
+        Plotly.newPlot("bubble", [bubbleChart], layout)
+     
+    });
+}
 
 // Function that initializes the dashboard
 function initialize()
 {
-    // let data = d3.json("samples.json")
-    // console.log(data);
-
     var select = d3.select("#selDataset");
     
     d3.json("samples.json").then((data) => {
         let sampleNames = data.names;
-        // console.log(sampleNames);
     
         sampleNames.forEach((sample) => {
             select.append("option")
@@ -58,6 +121,7 @@ function initialize()
         let firstSample = sampleNames[0];
         demoInfo(firstSample);
         buildBarChart(firstSample);
+        buildBubbleChart(firstSample)
     });
 
     
@@ -67,9 +131,8 @@ function initialize()
 function optionChanged(item)
 {
     demoInfo(item);
-    // console.log(item);
     buildBarChart(item);
+    buildBubbleChart(item);
 }
-
 
 initialize();
